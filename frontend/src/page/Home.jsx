a305cfc17fb8ade90c886e9d8b421419315e7f67
Alex Héritier <alex.heritier@outlook.fr>
2023-01-30 15:49:37 +0100

    Search function

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -48,6 +48,26 @@ const Home = () => {
 		fetchPosts();
 	}, []);
 
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		setSearchTimeout(
+			setTimeout(() => {
+				const searchResult = allPosts.filter(
+					(item) =>
+						item.name
+							.toLowerCase()
+							.includes(searchText.toLowerCase()) ||
+						item.prompt
+							.toLowerCase()
+							.includes(searchText.toLowerCase())
+				);
+				setSearchedResults(searchResult);
+			}, 500)
+		);
+	};
+
 	return (
 		<section className="max-w-7xl mx-auto">
 			<div>
@@ -61,7 +81,14 @@ const Home = () => {
 			</div>
 
 			<div className="mt-16">
-				<FormField />
+				<FormField
+					labelName="Search posts"
+					type="text"
+					name="text"
+					placeholder="Search something..."
+					value={searchText}
+					handleChange={handleSearchChange}
+				/>
 			</div>
 
 			<div className="mt-10">
@@ -83,7 +110,7 @@ const Home = () => {
 						<div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
 							{searchText ? (
 								<RenderCards
-									data={[]}
+									data={searchedResults}
 									title="No Search Results Found"
 								/>
 							) : (
