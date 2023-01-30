ed93b10e89c34a7ed11638d94836d91d41b8beb1
Alex Héritier <alex.heritier@outlook.fr>
2023-01-30 15:32:11 +0100

    Added community posts to home page

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -22,6 +22,32 @@ const Home = () => {
 	const [searchTimeout, setSearchTimeout] = useState(null);
 	const [searchedResults, setSearchedResults] = useState(null);
 
+	const fetchPosts = async () => {
+		setLoading(true);
+
+		try {
+			const response = await fetch("http://localhost:8080/api/v1/post", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (response.ok) {
+				const result = await response.json();
+				setAllPosts(result.data.reverse());
+			}
+		} catch (err) {
+			alert(err);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchPosts();
+	}, []);
+
 	return (
 		<section className="max-w-7xl mx-auto">
 			<div>
@@ -61,7 +87,10 @@ const Home = () => {
 									title="No Search Results Found"
 								/>
 							) : (
-								<RenderCards data={[]} title="No Posts Found" />
+								<RenderCards
+									data={allPosts}
+									title="No Posts Found"
+								/>
 							)}
 						</div>
 					</>
